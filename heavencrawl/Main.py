a2297c14ef4420f6a8677b523f3ed848b02120b6
AlionSSS <alionsss@foxmail.com>
2017-09-01 11:15:48 +0800

    Create Main.py
    爬虫程序入口，业务逻辑处理

---
+++ heavencrawl/Main.py
@@ -0,0 +1,53 @@
+# -*- encoding: utf-8 -*-
+
+"""
+
+ 天堂图片网爬虫Main
+ 
+ Author: ALion
+ 
+ Date: 2017/9/1 8:49
+"""
+
+import threading
+import threadpool
+import time
+import json
+from heavencrawl import HttpUtils, FileUtils
+from heavencrawl.Constants import *
+from heavencrawl.ImageSpider import parse_html
+
+
+def start(arg):
+    print('【' + threading.current_thread().name + ' -> start】')
+
+    url = BASE_URL + SECOND_URL + str(arg) + '.html'
+    html = HttpUtils.get_html(url)
+
+    for each in parse_html(html):
+        print(threading.current_thread().name + ' ->', each['name'], each['imgUrl'])
+        # 下载图片、保存
+        content = HttpUtils.get_bytes(each['imgUrl'])
+        path = DIR_NAME + '/' + each['imgDir'] + '/' + each['name'] + '.jpg'
+        FileUtils.save_file(path, content)
+        # 存储为json文本
+        mutexLock.acquire()
+        imageList.append(each)
+        FileUtils.save_text(DIR_NAME + '/images.json', json.dumps(imageList, ensure_ascii=False))
+        mutexLock.release()
+        # 暂停一下，再爬
+        time.sleep(SLEEP_TIME)
+
+    print('【' + threading.current_thread().name + ' -> over】')
+
+
+mutexLock = threading.Lock()
+imageList = []
+
+if __name__ == '__main__':
+    pool = threadpool.ThreadPool(THREAD_COUNT)
+    requests = threadpool.makeRequests(start, list(range(START_PAGE, END_PAGE + 1)))
+    [pool.putRequest(req) for req in requests]
+    pool.wait()
+
+    print('---main over---')
