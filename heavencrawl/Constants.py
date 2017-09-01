a25ef46872471c07b30e05b1f658dddcc3ae455b
AlionSSS <alionsss@foxmail.com>
2017-09-01 11:19:16 +0800

    Create Constants.py
    常量设置

---
+++ heavencrawl/Constants.py
@@ -0,0 +1,46 @@
+# -*- encoding: utf-8 -*-
+
+"""
+ 常量设置
+ 
+ Author: ALion
+ 
+ Date: 2017/8/31 19:01
+"""
+
+# 网址
+BASE_URL = 'http://www.ivsky.com'
+
+# 修改此处可以爬取其他类型图片(此处为自然风光)
+SECOND_URL = '/tupian/ziranfengguang/index_'
+
+# 存储路径
+DIR_NAME = './images'
+
+# User-Agent
+USER_AGENTS = [
+    'Mozilla/5.0(Macintosh;U;IntelMacOSX10_6_8;en-us)AppleWebKit/534.50(KHTML,likeGecko)Version/5.1Safari/534.50',
+    'Mozilla/5.0(Windows;U;WindowsNT6.1;en-us)AppleWebKit/534.50(KHTML,likeGecko)Version/5.1Safari/534.50',
+    'Mozilla/5.0(compatible;MSIE9.0;WindowsNT6.1;Trident/5.0;',
+    'Mozilla/5.0(Macintosh;IntelMacOSX10.6;rv:2.0.1)Gecko/20100101Firefox/4.0.1',
+    'Opera/9.80(WindowsNT6.1;U;en)Presto/2.8.131Version/11.11',
+    'Mozilla/5.0(Macintosh;IntelMacOSX10_7_0)AppleWebKit/535.11(KHTML,likeGecko)Chrome/17.0.963.56Safari/535.11',
+    'Mozilla/4.0(compatible;MSIE7.0;WindowsNT5.1;Maxthon2.0)',
+    'Mozilla/4.0(compatible;MSIE7.0;WindowsNT5.1;TencentTraveler4.0)',
+    'Mozilla/4.0(compatible;MSIE7.0;WindowsNT5.1;TheWorld)',
+    'Mozilla/5.0(Linux;U;Android2.3.7;en-us;NexusOneBuild/FRF91)AppleWebKit/533.1(KHTML,likeGecko)Version/4.0MobileSafari/533.1',
+    'Mozilla/5.0(iPad;U;CPUOS4_3_3likeMacOSX;en-us)AppleWebKit/533.17.9(KHTML,likeGecko)Version/5.0.2Mobile/8J2Safari/6533.18.5',
+    'Mozilla/4.0(compatible;MSIE7.0;WindowsNT5.1;Trident/4.0;SE2.XMetaSr1.0;SE2.XMetaSr1.0;.NETCLR2.0.50727;SE2.XMetaSr1.0)',
+]
+
+# 请求时间间隔
+SLEEP_TIME = 2.5
+
+# 线程数
+THREAD_COUNT = 8
+
+# 爬取该分类起始页
+START_PAGE = 1
+
+# 爬取该分类结束页
+END_PAGE = 10
