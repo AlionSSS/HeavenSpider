81f508f6d81c62040766dc4272401a31436958d7
AlionSSS <alionsss@foxmail.com>
2017-09-01 12:59:13 +0800

    Update LogUtils.py

--- heavencrawl/LogUtils.py
+++ heavencrawl/LogUtils.py
@@ -1,33 +1,47 @@
 # -*- encoding: utf-8 -*-
 
 """
- 日志工具
+ File 文件存储工具
  
  Author: ALion
  
- Date: 2017/9/1 2:29
+ Date: 2017/8/31 18:26
 """
-
-import time
 import os
 
 
-def log(path, text):
+def save_text(path, text):
     """
-    生成日志
+    文本存储
 
     :param path: 存储路径
-    :param text: 日志内容
+    :param text: 文本内容
     :return: None
     """
 
+    __check_path(path)
+
+    with open(path, 'w', encoding='utf-8') as file:
+        file.write(text)
+
+
+def save_file(path, content):
+    """
+    文件字节流存储
+
+    :param path: 存储路径
+    :param content: byte字节流
+    :return: None
+    """
+
+    __check_path(path)
+
+    with open(path, 'wb') as file:
+        file.write(content)
+
+
+def __check_path(path):
     file_name = path.split("/")[-1]
-    dir_path = path.strip(file_name)
+    dir_path = path.replace(file_name, '')
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
-
-    localtime = time.asctime(time.localtime(time.time()))
-    text = '\n' + localtime + ' ' + text
-
-    with open(path, 'a', encoding='utf-8') as file:
-        file.write(text)
